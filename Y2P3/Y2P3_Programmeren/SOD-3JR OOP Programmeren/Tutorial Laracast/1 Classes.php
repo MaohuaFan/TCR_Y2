5f72bfcc4cba008e50cbf4d3335acd56790083cc
MaohuaFan <9019910@student.zadkine.nl>
2024-03-07 10:16:36 +0100

    commit

--- Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Tutorial Laracast/1 Classes.php
+++ Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Tutorial Laracast/1 Classes.php
@@ -2,17 +2,27 @@
 
 class Task {
     public $description;
+    public $completed = false;
 
-    public function __construct()
+    public function __construct($description)
     {
         $this->description = $description;
     }
-
-
+    public function complete()
+    {
+        $this->completed = true;
+    }
 }
 
-$task = new Task('Learn OOP');
+$task = new Task('Learn OOP', 'The teacher bla bla bla...');
+$task2 = new Task('Pick up groceries');
+
+$task->complete();
+//$task2->complete();
 
 var_dump($task->description)
+var_dump($task2->description)
+var_dump($task->completed)
+var_dump($task2->completed)
 
 ?>
