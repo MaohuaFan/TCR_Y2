204e27a6caf41e9a662754d184de3efbab3e0c53
MaohuaFan <9019910@student.zadkine.nl>
2024-03-07 10:23:37 +0100

    commit

---
+++ Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Periode 7/Opdracht 1 Huizen/Huizen.php
@@ -0,0 +1,28 @@
+<?php
+
+class Task {
+    public $description;
+    public $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+    public function complete()
+    {
+        $this->completed = true;
+    }
+}
+
+$task = new Task('Learn OOP', 'The teacher bla bla bla...');
+$task2 = new Task('Pick up groceries');
+
+$task->complete();
+#$task2->complete();
+
+var_dump($task->description);
+var_dump($task2->description);
+var_dump($task->completed);
+var_dump($task2->completed);
+
+?>
--- Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Tutorial Laracast/1 Classes.php
+++ Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Tutorial Laracast/1 Classes.php
@@ -18,11 +18,11 @@ $task = new Task('Learn OOP', 'The teacher bla bla bla...');
 $task2 = new Task('Pick up groceries');
 
 $task->complete();
-//$task2->complete();
+#$task2->complete();
 
-var_dump($task->description)
-var_dump($task2->description)
-var_dump($task->completed)
-var_dump($task2->completed)
+var_dump($task->description);
+var_dump($task2->description);
+var_dump($task->completed);
+var_dump($task2->completed);
 
 ?>
