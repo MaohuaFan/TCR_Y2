de5c801585b582d686f5b0464882073c746571f9
MaohuaFan <9019910@student.zadkine.nl>
2024-03-11 12:50:05 +0100

    commit

--- Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Periode 7/Opdracht 6a Composer/src/login.php
+++ Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Periode 7/Opdracht 6a Composer/src/login.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        require "../vender/autoloader.php";
+        use Opdracht_6_Login\classes\User;
+        
+        // Is de login button aangeklikt?
+
+        if(isset($_POST['login-btn'])){}
+    ?>
 </body>
 </html>
