0a7137efd0c461bb4ba7acdcc406719189d2f108
MaohuaFan <9019910@student.zadkine.nl>
2024-03-11 11:33:36 +0100

    commit

--- Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Periode 7/Opdracht 1 Huizen/Huizen.php
+++ Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Periode 7/Opdracht 1 Huizen/Huizen.php
@@ -1,51 +1,45 @@
 <?php
 
     class House {
-
+        
         // Attributes
         protected int $floor;
         protected int $rooms;
         protected float $width;
         protected float $height;
         protected float $depth;
+        protected int $volume;
 
         // Constructor
-        public function __construct($floor, $rooms, $width, $height, $depth)
-        {
+        public function __construct($floor, $rooms, $width, $height, $depth) {
             $this->floor = $floor;
             $this->rooms = $rooms;
             $this->width = $width;
             $this->height = $height;
             $this->depth = $depth;
+            $this->setVolume();
+        }
+        
+        // Methods
+        public function setVolume() {
+            $this->volume = $this->width * $this->height * $this->depth;
         }
 
-        public function __construct($volume)
-        {            
-            $this->volume = ;
+        public function getPrice() {
+            return "De prijs van het huis is: " . ($this->volume * 1500);
+        }
+
+        public function getHouse() {
+            return "Dit huis heeft " . $this->floor . " verdiepingen, " . $this->rooms . " kamers en heeft een volume van " . $this->volume . "m3 <br>";        
         }
     }
 
-    // Methods
-    public function setVolume($volume) {
-        protected int $volume = $width * $height * $depth;
-        $this->volume = $volume;
-    }
+    $House1 = new House(2, 4, 5, 5, 4);
+    $House2 = new House(3, 6, 5, 5, 6);
+    $House3 = new House(2, 3, 5, 5, 3);
 
-    public function getHouse() {
-
-    }
-
-    public function getPrice() {
-
-    }
-
-    $House1 = new House('2','4','2');
-    
-    $john->age = 30;
-    #$john->age = 15;
-
-    var_dump($john);
-
-
+    echo $House1->getHouse(). $House1->getPrice(). "<br>";
+    echo $House2->getHouse(). $House2->getPrice(). "<br>";
+    echo $House3->getHouse(). $House3->getPrice(). "<br>";
 
 ?>
