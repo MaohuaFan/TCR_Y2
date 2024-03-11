47c82faa666f415c0f737e9ccc30b3f2ec46531f
MaohuaFan <9019910@student.zadkine.nl>
2024-03-11 12:09:29 +0100

    commit

---
+++ Y2P3/Y2P3_Programmeren/SOD-3JR OOP Programmeren/Periode 7/Opdracht 6a Composer/src/classes/user.php
@@ -0,0 +1,14 @@
+<?php
+    //Functie: 
+    //Autheur: 
+
+    namespace Opdracht_6_Login\classes;
+
+    class User extends Database{
+
+        //Eigenschappen
+        public $username;
+        public $email;
+        public $password;
+    }
+?>
